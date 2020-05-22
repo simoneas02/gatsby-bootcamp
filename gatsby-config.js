a7b93453a304b482cafdddeb8ed851b5cd78bed5
simoneas02 <simoneas02@hotmail.com>
2020-05-21 23:34:01 -0300

    add contentful data

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: { name: "src", path: `${__dirname}/src/` },
     },
