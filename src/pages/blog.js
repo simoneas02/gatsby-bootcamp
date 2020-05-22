e3fa0b41c0f71ba2c452fa41db5122d193cf1d14
simoneas02 <simoneas02@hotmail.com>
2020-05-22 00:21:28 -0300

    render contentful posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,15 @@ import Layout from "../components/layout"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
+      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
+        edges {
+          node {
+            title
+            slug
+            publishDate(formatString: "MMMM Do, YYY")
+          }
+        }
+      }
       allMarkdownRemark {
         edges {
           node {
@@ -33,6 +42,14 @@ const BlogPage = () => {
             <Link to={`/blog/${edge.node.fields.slug}`}>
               <h2>{edge.node.frontmatter.title}</h2>
               <p>{edge.node.frontmatter.date}</p>
+            </Link>
+          </li>
+        ))}
+        {data.allContentfulBlogPost.edges.map((edge, index) => (
+          <li key={index}>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishDate}</p>
             </Link>
           </li>
         ))}
